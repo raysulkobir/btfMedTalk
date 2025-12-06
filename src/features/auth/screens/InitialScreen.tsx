b1d6bbf414d6c81c76e406c8debd42bb6054d8e4
Md.Raysul kobir <raysul@thals.org>
2025-12-06 15:39:13 +0600

    wip

--- src/features/auth/screens/InitialScreen.tsx
+++ src/features/auth/screens/InitialScreen.tsx
@@ -5,16 +5,16 @@ import SplashScreen from 'react-native-splash-screen';
 type Props = { navigation: any };
 
 export default function InitialScreen({ navigation }: Props) {
-    useEffect(() => {
-        SplashScreen.hide();
-        const id = setTimeout(() => {
+    // useEffect(() => {
+        // SplashScreen.hide();
+        // const id = setTimeout(() => {
             // Clear stack so user can't go back to InitialScreen
-            navigation.reset({ index: 0, routes: [{ name: 'OnboardingTour' }] });
-        }, 400);
+            // navigation.reset({ index: 0, routes: [{ name: 'OnboardingTour' }] });
+        // }, 400);
 
         // Cleanup: if this screen unmounts before 400ms, cancel the timer
-        return () => clearTimeout(id);
-    }, [navigation]);
+        // return () => clearTimeout(id);
+    // }, [navigation]);
 
     return (
         <View style={styles.container}>
